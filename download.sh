ffd25f37a2410f1e95ccac76b4816aaae1dccc0b
Vadim Tkachenko <vadim@percona.com>
2017-03-18 18:02:53 -0700

    New URL to download data

--- download.sh
+++ download.sh
@@ -1,9 +1,9 @@
 #download database
 
-for s in `seq 1987 2015`
+for s in `seq 1987 2017`
 do
 for m in `seq 1 12`
 do
-wget http://tsdata.bts.gov/PREZIP/On_Time_On_Time_Performance_${s}_${m}.zip
+wget http://transtats.bts.gov/PREZIP/On_Time_On_Time_Performance_${s}_${m}.zip
 done
 done
