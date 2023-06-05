1a101c65dbd5d662edfa08a5580a905c23e30b6f
Vadim Tkachenko <290907+vadimtk@users.noreply.github.com>
2023-06-05 12:58:54 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,6 +4,6 @@ for s in `seq 1987 2018`
 do
 for m in `seq 1 12`
 do
-wget https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${s}_${m}.zip
+wget --no-check-certificate https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${s}_${m}.zip
 done
 done
